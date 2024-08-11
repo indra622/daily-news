d3fe1a6ad102656b22e185f89f3fe5d68064248d
Hosung Park <indra622@gmail.com>
2024-08-11 21:36:00 +0900

    chore: Update backend to use httpx for asynchronous requests

--- backend/main.py
+++ backend/main.py
@@ -1,33 +1,37 @@
 from fastapi import FastAPI
-import requests
+import httpx
 from bs4 import BeautifulSoup
 
 app = FastAPI()
 
 archive_url = "https://newsletter.towardsai.net/archive"
 
-def fetch_article_links(archive_url):
-    response = requests.get(archive_url)
+async def fetch_article_links(archive_url):
+    async with httpx.AsyncClient() as client:
+        response = await client.get(archive_url)
+        # print(f'RESPONSE: {response}')
     soup = BeautifulSoup(response.content, 'html.parser')
     article_links = []
     for a_tag in soup.find_all('a', href=True):
         href = a_tag['href']
-        if href.startswith("https://newsletter.towardsai.net/c/"):
+        if href.startswith("https://newsletter.towardsai.net/p/"):
             article_links.append(href)
     return article_links
 
-def fetch_article_content(url):
-    response = requests.get(url)
+async def fetch_article_content(url):
+    async with httpx.AsyncClient() as client:
+        response = await client.get(url)
     soup = BeautifulSoup(response.content, 'html.parser')
-    content = soup.find('div', class_='email-content').get_text(separator=" ").strip()
+    content = soup.find('div', class_='body markup').get_text(separator=" ").strip()
     return content
 
 @app.get("/api/articles")
 async def get_articles():
-    article_links = fetch_article_links(archive_url)
+    article_links = await fetch_article_links(archive_url)
+    print(f'LINKS:{article_links}')
     articles = []
     for link in article_links:
-        content = fetch_article_content(link)
+        content = await fetch_article_content(link)
         articles.append({
             "url": link,
             "content": content
